07c66aff62c948ce2b24d95cc8ca6f1ad541cb4e
Cory-Accardo <accardo@usc.edu>
2021-03-28 18:20:10 -0700

    more mobile functionality

--- api/search.js
+++ api/search.js
@@ -7,8 +7,6 @@ const client = yelp.client(apiKey);
 
 
 module.exports = (req, res) => {
-  console.log('HELLO!!!! DID I AT LEAST GET HERE?')
-  console.log(req)
   return new Promise(resolve => { 
     const category = JSON.parse(req.query.param1)
     const location = JSON.parse(req.query.param2)
--- components/Searchbar.js
+++ components/Searchbar.js
@@ -1,19 +1,41 @@
 import styles from '../styles/Searchbar.module.css'
 import LandIcon from '../assets/svg/landmark_icon.svg'
 import SearchIcon from '../assets/svg/landmark_icon.svg'
+import {useEffect} from 'react'
+
 
 function Searchbar(props) {
-  const handleWindowChange = () => {
-    const mobileWindow = window.matchMedia("(max-width: 600px)");
-    if(mobileWindow.matches){
-      if(props.param == "business"){
-        return <SearchIcon/>
+  useEffect(() => { // handles search bar transition from mobile to desktop. 
+    function handleResize(){
+      const searchBar = document.getElementsByClassName(styles.input)
+      if(window.innerWidth < 600){ // sets mobile static functionality and adds icons to relevant positions
+        for (let i = 0; i < searchBar.length; i++){
+          searchBar[i].style.width = '10em'
+          if(searchBar[i].alt == "location,search"){
+            searchBar[i].placeholder = "enter location"
+          }
+          else if(searchBar[i].alt == "business,search"){
+            searchBar[i].placeholder = "enter query"
+          }
+        }
       }
-      else if(props.param == "location"){
-        return <LandIcon/>
+      else{ // readjusts to dekstop functionality
+        for (let i = 0; i < searchBar.length; i++){
+          searchBar[i].placeholder = ''
+          if(searchBar[i].value.length < 1){
+            searchBar[i].style.width = '2em'
+          }
+          else if(searchBar[i].value.length < 29){
+            searchBar[i].style.width = `${1 + .39*searchBar[i].value.length}em`
+          }
+        }
       }
-      }
-  }
+    }
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize' , handleResize)
+    }
+  })
   const handleSearchSpace = (e =>{
     const mobileWindow = window.matchMedia("(max-width: 600px)");
     if(!mobileWindow.matches){
@@ -26,10 +48,14 @@ function Searchbar(props) {
         nodeElem.style.width = `${1 + .39*text.length}em`
       }
     }
+    else if(mobileWindow.matches){
+      const nodeElem = e.target;
+      nodeElem.style.width = '10em'
+    }
   })
   return (
     <div className={styles.Searchbar}>
-      <input spellCheck = "false" maxLength="29" className={styles.input} alt = {[[props.param],[props.url]]} type="text" onChange={e => {
+      <input id = "input_item" spellCheck = "false" maxLength="29" className={styles.input} alt = {[[props.param],[props.url]]} type="text" onChange={e => {
         props.changeInput(e)
         handleSearchSpace(e)
         }}/>
